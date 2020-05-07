4fb746eb4b87ea25ca25066c6bb0a37cdc6e00fd
LEE SANG AH <elfish97@naver.com>
2020-05-07 11:00:00 +0900

    test application commit

---
+++ ARguide/Assets/Scripts/GPSMgr.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPSMgr : MonoBehaviour
+{
+
+    // VAR : GET loation info
+    private Text GPSText;
+    private bool gpsInit;
+    // location 
+    private static LocationInfo LOC;
+    private double LAT;
+    private double LON;
+    private float compass_headingAccu;
+    public float compass_trueHeading;
+    private int validCount;
+    // tmp obj that help to set GPS text
+    private string LOCtext;
+
+
+    // VAR : GET lat and lon from INPUTFIELD
+    private InputField enteredLAT;
+    private InputField enteredLON;
+    private Button enterBtn;
+    private Text enterStatus;
+    private Text guideStatus;
+
+    // VAR : 화살표로 안내할 타겟위치의 위도경도
+    public double targetLAT = 0;
+    public double targetLON = 0;
+    private bool isGuiding = false;
+    private int guideCount = 0;
+
+    // VAR : ARCamera 관련 변수
+    private Camera ARCamera;
+    public Transform ARCameraTransform;
+
+    private float Heading;              // 이 각도로 배치한 사물은 북쪽을 가르킵니다!arcamera 각도 교정용
+    public GameObject obj_Compass;      // 나침반
+    public GameObject obj_GuideArrow;   // 방향안내용
+
+    private IEnumerator GPSloader;
+
+
+    [Header ("나침반 오브젝트")]
+    [SerializeField] public GameObject compassObj;
+
+    [Header ("안내용 화살표")]
+    [SerializeField] public GameObject guideArrow;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 카메라 받기
+        ARCamera = GameObject.Find("First Person Camera").GetComponent<Camera>();
+        ARCameraTransform = GameObject.Find("First Person Camera").transform;
+
+        // 받아온 gps 방향정보 띄워주는 텍스트
+        GPSText = transform.Find("Canvas").Find("GPSText").GetComponent<Text>();
+
+        // targetLATLON 받아오는 InputField 및 버튼과 디버깅용 텍스트오브젝트 불러오기
+        enteredLAT = transform.Find("Canvas").Find("LATInput").GetComponent<InputField>();
+        enteredLON = transform.Find("Canvas").Find("LONInput").GetComponent<InputField>();
+        enterBtn = transform.Find("Canvas").Find("Button").GetComponent<Button>();
+        enterBtn.onClick.AddListener(Check_enteredValue);
+        enterStatus = transform.Find("Canvas").Find("Enterstatus").GetComponent<Text>();
+        guideStatus = transform.Find("Canvas").Find("Guidestatus").GetComponent<Text>();
+
+
+        // GPS 측정 시작
+        Input.location.Start(0.01f, 0.01f); // Accuracy of 0.01 m
+        Input.compass.enabled = true;
+       
+        int wait = 1000; // Per default
+       
+        // Checks if the GPS is enabled by the user (-> Allow location )
+        if(!Input.location.isEnabledByUser){
+            GPSText.text = "GPS not available !!";
+            
+        } else {
+            GPSText.text = "GPS is available !!";
+            while(Input.location.status == LocationServiceStatus.Initializing && wait>0){
+                wait--;
+            }
+            if (Input.location.status == LocationServiceStatus.Failed) {
+                GPSText.text = "GPS get FAILED";
+            } else {
+                gpsInit = true;
+                GPSloader=LoadGPS(1f);
+                StartCoroutine(GPSloader);
+            }
+        }
+        // 나침반을 카메라의 child 로 생성
+        obj_Compass = Instantiate(compassObj, ARCameraTransform.position, Quaternion.identity, ARCameraTransform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ARCamera의 각도 - 나침반 각도 계산
+        // 이 각도로 배치한 사물은 북쪽을 가리킵니다!
+        Heading = ARCameraTransform.eulerAngles.y - compass_trueHeading;
+        // 나침반 각도 업데이트 (lerp 추가)
+        //obj_Compass.transform.rotation = Quaternion.Euler(0, Heading, 0);
+        obj_Compass.transform.rotation = Quaternion.Lerp(obj_Compass.transform.rotation, Quaternion.Euler(0, Heading, 0), 0.5f);
+
+        // 안내중이지 않고, targetLATLON 값이 있을 때 코루틴 시작!
+        if(isGuiding == false){
+            if(targetLAT != 0 && targetLON !=0 ){
+                isGuiding = true;
+                StartCoroutine(GuideToTarget());
+            }
+        }
+        
+    }
+
+    // GPS 좌표와 각도를 계속 로드해 ui에 표시해주는 코루틴
+    private IEnumerator LoadGPS(float waitTime){
+        while (Input.location.isEnabledByUser){
+            // 정해진 초마다 gps 체크
+            yield return new WaitForSeconds(waitTime);
+
+            if(Input.location.isEnabledByUser){
+                // 좌표 및 방향 확인
+                LOC = Input.location.lastData;
+                LAT = LOC.latitude;
+                LON = LOC.longitude;
+                compass_headingAccu = Input.compass.headingAccuracy;
+                compass_trueHeading = Input.compass.trueHeading;
+                validCount++;
+
+                LOCtext = "GPS is available ! vC:"+validCount;
+                LOCtext += "\nstatus: "+Input.location.status;
+                LOCtext += "\nLAT: "+LAT;
+                LOCtext += "\nLON: "+LON;
+                LOCtext += "\ncompAccu: "+compass_headingAccu;
+                LOCtext += "\ncompHead: "+compass_trueHeading;
+                GPSText.text = LOCtext;
+
+            } else {
+                GPSText.text = "GPS not available !"+"\nLAT: "+"\nLON:";
+            }
+        }
+    }
+
+    // 버튼 눌릴 시 실행되는 메서드
+    // InputField로 받은 위도경도값이 적절하면 targetLATLON 값으로 설정
+    // 적절하지 않을 경우 0으로 설정
+    private void Check_enteredValue(){
+        string tmpText = "";
+        //string tmpLAT = enteredLAT.text;
+        //string tmpLON = enteredLON.text;
+        double tmpLAT;
+        double tmpLON;
+
+        try{
+            tmpLAT = double.Parse(enteredLAT.text);
+            tmpLON = double.Parse(enteredLON.text);
+
+            if (37<tmpLAT && tmpLAT<38 && 126<tmpLON && tmpLON<128){
+                tmpText += "valid point";
+                tmpText += "\nNOW GUIDING";
+                targetLAT = tmpLAT;
+                targetLON = tmpLON;
+            } else {
+                tmpText += "\n invalid point";
+                tmpText += "\n LAT37~38 LON126~128 needed";
+                targetLAT = 0;
+                targetLON = 0;
+            }
+
+        } catch (Exception e) {
+            tmpText = "ERR during parse str -> double";
+        }
+
+        // set status text
+        enterStatus.text = tmpText;
+
+    }
+
+    // 현재 설정된 targetLATLON 의 방향으로 화살표를 띄워줌
+    // targetLANLON 값이 있으면 이 코루틴이 시작되고. 화살표 생성하여 안내
+    // targetLANLON 값이 없으면 화살표 삭제, 안내 중지
+    private IEnumerator GuideToTarget(){
+
+        // 안내용 arrow를 카메라의 child 로 생성
+        obj_GuideArrow = Instantiate(guideArrow, ARCameraTransform.position, Quaternion.identity, ARCameraTransform);
+
+        
+
+        // create guiding arrow
+        while (targetLAT != 0 && targetLON != 0){
+            yield return new WaitForSeconds(0.1f);
+            guideCount++;
+            guideStatus.text = "\ngC="+guideCount;
+
+
+            // 나침반상에서 targetPoint가 어느 방향에 있는지 구하기
+            float targetAngle = Mathf.Atan2((float)targetLON-(float)LON, (float)targetLAT-(float)LAT) * Mathf.Rad2Deg;
+
+            obj_GuideArrow.transform.rotation = Quaternion.Lerp(obj_GuideArrow.transform.rotation, Quaternion.Euler(0, Heading+targetAngle, 0), 0.5f);
+            //obj_GuideArrow.transform.LookAt(targetPoint);
+
+            // 로그 찍기
+            guideStatus.text += "\ntAngle: "+targetAngle;
+            guideStatus.text += "\n\nnow guide \nFROM LAT "+sosu2(LAT)+" LON "+sosu2(LON);
+            guideStatus.text += "\nTO LAT "+sosu2(targetLAT)+" LON "+sosu2(targetLON);
+            //guideStatus.text += "\nx: "+obj_GuideArrow.transform.eulerAngles.x;
+            //guideStatus.text += "\ny: "+obj_GuideArrow.transform.eulerAngles.y;
+            //guideStatus.text += "\nz: "+obj_GuideArrow.transform.eulerAngles.z;
+
+            
+
+            
+
+        }
+        // targetLATLON의 값이 0으로 변경될 경우 guidingarrow 삭제하고 안내 중지
+        Destroy(obj_GuideArrow);
+        guideStatus.text = "stop guide";
+        isGuiding = false;
+    }
+
+    // 소수점 둘째자리 이하 버리는 메서드
+    float sosu2 (double value){
+        return (float)Math.Truncate(value*100)/100;
+    }
+
+
+}
+
---
+++ ARguide/Assets/Scripts/TouchMgr.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleARCore;
+
+
+public class TouchMgr : MonoBehaviour
+{
+
+    private Camera ARCamera;
+    private Text ARCamText;
+    private IEnumerator ARCamchecker;
+
+    private string statusText;
+    public Transform ARCameraTransform;
+
+    private int frameCount;
+
+
+    [Header ("클릭시 생성되는 오브젝트")]
+    public GameObject placeObject;
+    
+    [Header ("따라다니는 오브젝트")]
+    public GameObject followObject;
+
+    public GameObject followArrow;
+
+    [Header ("GPS관리자")]
+    [SerializeField] public GPSMgr GPSMgr;
+
+    // 이 각도로 배치한 사물은 북쪽을 가르킵니다!
+    // arcamera 각도 교정용
+    private float Heading;
+
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+            // arcore device 프리팹 하위의 카메라를 찾아 변수에 할당
+            ARCamera = GameObject.Find("First Person Camera").GetComponent<Camera>();
+            ARCameraTransform =  GameObject.Find("First Person Camera").transform;
+
+            // 디버깅용 arcamera 위치 텍스트
+            ARCamText = transform.Find("Canvas").Find("ARCamText").GetComponent<Text>();
+            //ARCamchecker=checkARCamera(1.0f);
+            //StartCoroutine(ARCamchecker);
+            
+            // followObject 생성
+            //followArrow = Instantiate(followObject, ARCameraTransform.position, Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frameCount++;
+
+        // ARCamera의 각도 - 나침반 각도 계산
+        // 이 각도로 배치한 사물은 북쪽을 가리킵니다...
+        Heading = ARCameraTransform.eulerAngles.y - GPSMgr.compass_trueHeading;
+
+        // uitext 업데이트
+        statusText = "...Camera fC: "+frameCount;
+        statusText += "\nposX: "+ARCameraTransform.position.x;
+        statusText += "\nposY: "+ARCameraTransform.position.y;
+        statusText += "\nposZ: "+ARCameraTransform.position.z;
+        statusText += "\n";
+        statusText += "\nrotX: "+ARCameraTransform.eulerAngles.x;
+        statusText += "\nrotY: "+ARCameraTransform.eulerAngles.y;
+        statusText += "\nrotZ: "+ARCameraTransform.eulerAngles.z;
+        statusText += "\n";
+        ARCamText.text = statusText;
+
+        // 일단 followArrow 위치 및 방향 부터 업데이트
+        //followArrow.transform.position = ARCameraTransform.position;
+        //followArrow.transform.rotation = Quaternion.Euler(0, Heading, 0);
+        // 너무 뚝뚝끊겨서 lerp 넣어줌
+        //followArrow.transform.position = Vector3.Lerp(followArrow.transform.position, ARCameraTransform.position, 0.5f);
+        //followArrow.transform.rotation = Quaternion.Lerp(followArrow.transform.rotation, Quaternion.Euler(0, Heading, 0), 0.5f);
+        
+
+        // 터치 처리
+        if(Input.touchCount==0){
+            return;
+        } else {
+            // 첫번재 터치 정보 추출
+            Touch touch = Input.GetTouch(0);
+
+            // Arcore에서 제공하는 raycastHit와 유사한 구조체
+            TrackableHit hit;
+
+            // 검출 대상을 평면 또는 Feature Point 로 한정
+            TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon | TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+            // 터치한 지점으로 ray 발사
+            if (touch.phase == TouchPhase.Began
+                && Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+                {
+                    // 객체를 고정할 앵커 생성
+                    var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                    // 객체 생성
+                    GameObject obj = Instantiate(placeObject, hit.Pose.position, Quaternion.identity, anchor.transform);
+                    
+                    // 생성한 객체가 사용자쪽을 바라보도록 회전값 계산
+                    //var rot = Quaternion.LookRotation(ARCamera.transform.position - hit.Pose.position);
+
+                    // 사용자 쪽 회전값 적용
+                    //obj.transform.rotation = Quaternion.Euler(ARCamera.transform.position.x, rot.eulerAngles.y, ARCamera.transform.position.z);
+                    obj.transform.rotation = Quaternion.Euler(0, Heading, 0);
+                    
+                }
+        }
+    }
+
+    private IEnumerator checkARCamera(float waitTime){
+        while (true){
+            // 정해진 초마다 ARcamera 체크
+            yield return new WaitForSeconds(waitTime);
+
+            
+
+            statusText = "...Camera";
+            statusText += "\nposX: "+ARCameraTransform.position.x;
+            statusText += "\nposY: "+ARCameraTransform.position.y;
+            statusText += "\nposZ: "+ARCameraTransform.position.z;
+            statusText += "\n";
+            statusText += "\nrotX: "+ARCameraTransform.eulerAngles.x;
+            statusText += "\nrotY: "+ARCameraTransform.eulerAngles.y;
+            statusText += "\nrotZ: "+ARCameraTransform.eulerAngles.z;
+            statusText += "\n";
+
+            ARCamText.text = statusText;
+
+
+
+        }
+    }
+}
